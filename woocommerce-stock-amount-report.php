2956e35b1109106ec3ee7b96fa2a50397f7f518e
Thiago Guimaraes <thiagogsr@gmail.com>
2017-03-16 02:26:08 -0300

    Add report to stock reports nav

--- woocommerce-stock-amount-report.php
+++ woocommerce-stock-amount-report.php
@@ -27,3 +27,57 @@ if (!defined('ABSPATH')) {
 if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
   return;
 }
+
+/**
+* # WooCommerce Stock Amount Report Main Plugin Class
+*
+* ## Plugin Overview
+*
+* This plugin adds a new report to WooCommerce a new section in the WooCommerce Reports -> Stocks area called 'Stock amount'.
+* The report visualizes how much you have in your available stock if you sell everything.
+*
+*/
+class WC_StockAmount_Report {
+  /** plugin version number */
+  public static $version = '0.0.1';
+
+  /** @var string the plugin file */
+  public static $plugin_file = __FILE__;
+
+  /** @var string the plugin file */
+  public static $plugin_dir;
+
+  /**
+  * Initializes the plugin
+  *
+  * @since 0.0.1
+  */
+  public function init() {
+    global $wpdb;
+
+    self::$plugin_dir = dirname(__FILE__);
+
+    // Add the reports layout to the WooCommerce -> Reports admin section
+    add_filter('woocommerce_admin_reports',  __CLASS__ . '::initialize_location_admin_report', 12, 1);
+  }
+
+  /**
+  * Add our location report to the WooCommerce stock reports array.
+  *
+  * @param array Array of All Report types & their labels
+  * @return array Array of All Report types & their labels, including the 'Stock amount' report.
+  * @since 0.0.1
+  */
+  public static function initialize_location_admin_report($report) {
+    $report['stock']['reports']['stock_amount'] = array(
+      'title'       => __('Sales by location', 'woocommerce-location-report'),
+      'description' => '',
+      'hide_title'  => true,
+      'callback'    => array('WC_Admin_Reports', 'get_report'),
+    );
+
+    return $report;
+  }
+}
+
+WC_StockAmount_Report::init();
